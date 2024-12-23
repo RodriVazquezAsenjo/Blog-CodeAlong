4ba1a677c9f2dce617d0829d18ce1683ab64954c
Rodrigo Vazquez Asenjo <r.vazquezasenjo@gmail.com>
2024-12-23 16:38:01 +0000

    Started working on the views"

--- blog/models.py
+++ blog/models.py
@@ -14,6 +14,13 @@ class Post(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices = STATUS, default = 0)
     excerpt = models.TextField(blank = True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return f"The title of this post is {self.title} | written by {self.author}"
 
 
 class Comment(models.Model):
@@ -30,3 +37,10 @@ class Comment(models.Model):
     body = models.TextField()
     approved = models.BooleanField(default = False),
     created_On = models.DateTimeField(auto_now_add = True)
+    challenge = models.SlugField()
+
+    class Meta:
+        ordering = ["created_On"]
+    
+    def __str__(self):
+        return f"Comment {self.body} by {self.author}"
--- codestar/settings.py
+++ codestar/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3u0t*9d28cb^z472c0fb=*cp+t1um)$9i-k@4fhj-w#wh)155f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['8000-rodrivazque-blogcodealo-e40tsm2rk10.ws.codeinstitute-ide.net', '.herokuapp.com']
+ALLOWED_HOSTS = ['8000-rodrivazque-blogcodealo-s0x9etjl5hj.ws.codeinstitute-ide.net', '.herokuapp.com']
 
 
 # Application definition

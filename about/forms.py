c29eac02f42be005966e22b29fc4fbf6241b3021
Rodrigo Vazquez Asenjo <r.vazquezasenjo@gmail.com>
2024-12-30 17:17:27 +0000

    oeiurg

--- about/forms.py
+++ about/forms.py
@@ -1,4 +1,4 @@
-from .models import Comment
+from .models import CollaborateRequest
 from django import forms
 
 
--- about/views.py
+++ about/views.py
@@ -1,15 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import 
+from django.contrib import messages
 from .models import About
+from .forms import CollaborateForm
 
 
 def about_me(request):
     """
     Renders the About page
     """
+    if request.method == 'POST':
+        collaborate_form = CollaborateForm(data = request.POST)
+        if collaborate_form.is_valid:
+            collaborate_form.save()
+            message.add_message(
+                request, message.SUCCESS,
+                'Collaboration request received! I endeavour to respond within 2 working days.'
+            )
+
     about = About.objects.all().order_by('-updated_on').first()
+
+    collaborate_form = CollaborateForm
 
     return render(
         request,
         "about/about.html",
-        {"about": about},
+        {
+            "about": about,
+            "colaborate_form": collaborate_form,
+            },
+
     )
